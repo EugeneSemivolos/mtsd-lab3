b5212bd55c39f3b9749719bcfbe491b877d5e5dd
EugeneSemivolos <semivolos.evgen1@gmail.com>
2023-04-04 08:59:17 +0300

    added endpoint api.py and numpy dependence

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,3 +1,4 @@
+import numpy as np
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -6,3 +7,12 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+@router.get('/matrix_mult')
+def matrix() -> dict:
+    matrix_a = np.random.rand(10, 10).tolist()
+    matrix_b = np.random.rand(10, 10).tolist()
+    product = np.dot(matrix_a, matrix_b).tolist()
+    result = {"matrix_a": matrix_a, "matrix_b": matrix_b, "product": product}
+    return result
